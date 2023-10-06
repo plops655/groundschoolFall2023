0df6dd9cd68ee0effa1f19c7767a2a8bb25d5406
John Cole Lomax <jclomax25@berkeley.edu>
2023-10-06 15:21:25 -0700

    Added Image parser function

---
+++ ImageArgumentParser.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main(arguments):
+    print("Starting main() function")
+
+# This if statement will take a path to an image if you attach the flag -ip before the call.
+# It will then run the main function passing in the arguments as a list, so to access the image path, you must call arguments[0]
+# You can import your solution and then call it in the main() function
+# Example call in terminal to run this script: python3 ImageArgumentParser.py -ip "./test_images/color.png"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--img_path', type=str, help='The path of the image you want to run color detection on.')
+    args = parser.parse_args()
+    main(args)
